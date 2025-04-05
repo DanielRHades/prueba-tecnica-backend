98921be100f7664d3175c1585ef2ce572cc3212d
Daniel Rodriguez <drodriguez442@unab.edu.co>
2025-04-04 19:57:03 -0500

    Prueba Unitaria Jest Implementada
    Se utilizo la Query solicitada de Countries para la creación de la prueba unitaria de Jest, la cual paso correctamente.

---
+++ __tests__/countries.test.ts
@@ -0,0 +1,66 @@
+import { countryResolvers } from '../graphql/countries/countries.resolvers';
+import { Context } from '../graphql/context';
+import { PrismaClient } from '@prisma/client';
+
+jest.mock('@prisma/client', () => {
+  const actualPrisma = jest.requireActual('@prisma/client');
+  return {
+    ...actualPrisma,
+    PrismaClient: jest.fn().mockImplementation(() => ({
+      $queryRaw: jest.fn().mockResolvedValue([
+        { id: '1', name: 'Country 1' },
+        { id: '2', name: 'Country 2' },
+      ]),
+      $connect: jest.fn(),
+      $disconnect: jest.fn(),
+      $use: jest.fn(),
+      $on: jest.fn(),
+    })),
+  };
+});
+
+jest.mock('../graphql/utils/errors', () => ({
+  requireRole: jest.fn(),
+}));
+
+enum Enum_RoleName {
+  Admin = "Admin",
+  Manager = "Manager",
+  User = "User"
+}
+
+describe('countryResolvers', () => {
+  let context: Context;
+
+  beforeEach(() => {
+    context = {
+      prisma: new PrismaClient(),
+      user: {
+        id: '1',
+        name: "Daniel",
+        email: 'daniel@test.com',
+        roleId: 'roleId',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        emailVerified: null,
+        termsAndConditionsAccepted: null,
+        image: null,
+        position: null,
+        Role: {
+          id: 'roleId',
+          name: Enum_RoleName.Admin,
+          createdAt: new Date(),
+        },
+        Country: [],
+      },
+    };
+  });
+
+  it('Debe regresar todos los countries.', async () => {
+    const result = await countryResolvers.Query.countries(null, { take: 2, skip: 0 }, context);
+    
+    expect(result).toHaveLength(2);
+    expect(result[0].name).toBe('Country 1');
+    expect(result[1].name).toBe('Country 2');
+  });
+});
