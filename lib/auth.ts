7999ccf6667b0673f3ffd07a6e496574ef86e01e
Daniel Rodriguez <drodriguez442@unab.edu.co>
2025-05-07 09:11:32 -0500

    refactor(auth.ts): update getUserFromToken function to use arrow function syntax for consistency and readability style(auth.ts): standardize string quotes from double to single quotes for consistency style(prisma.ts): standardize string quotes from double to single quotes for consistency

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,29 +1,32 @@
-import { NextApiRequest } from "next";
-import { prisma } from './prisma'
-import jwt from "jsonwebtoken";
+import { NextApiRequest } from 'next';
+import { prisma } from '@/lib/prisma';
+import jwt from 'jsonwebtoken';
 
-export async function getUserFromToken(req: NextApiRequest) {
-    const token = req.headers.authorization?.replace("Bearer ", "");
+export const getUserFromToken = async (req: NextApiRequest) => {
+  const token = req.headers.authorization?.replace('Bearer ', '');
 
-    if (!token) throw new Error("Acceso denegado: Token requerida.");
+  if (!token) throw new Error('Acceso denegado: Token requerida.');
 
-    let decoded: any;
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-    } catch (err) {
-        throw new Error("Token inválida o expirada.");
-    }
+  let decoded: any;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+  } catch (err) {
+    throw new Error('Token inválida o expirada.');
+  }
 
-    const session = await prisma.session.findUnique({
-        where: { sessionToken: token },
-        include: { User: { include: { Role: true, Country: true } } },
-    });
+  const session = await prisma.session.findUnique({
+    where: { sessionToken: token },
+    include: { User: { include: { Role: true, Country: true } } }
+  });
 
-    if (!session || !session.User) throw new Error("Acceso denegado: Sesión no válida.");
+  if (!session || !session.User)
+    throw new Error('Acceso denegado: Sesión no válida.');
 
-    if (session.User.email !== decoded.Email) {
-        throw new Error("Acceso denegado: El correo del token no coincide con el del usuario.");
-    }
+  if (session.User.email !== decoded.Email) {
+    throw new Error(
+      'Acceso denegado: El correo del token no coincide con el del usuario.'
+    );
+  }
 
-    return session.User;
-}
+  return session.User;
+};
